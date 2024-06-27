0c23a42f23c948a4601a5e59417547812d762f65
developer2031997 <168425489+developer2031997@users.noreply.github.com>
2024-06-27 17:35:55 +0530

    first

---
+++ pages/contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h1 class="py-5 text-4xl">Contact Page ..</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './layouts/**/*.html',
+    './content/**/*.md',
+    './content/**/*.html',
+    './src/**/*.ts',
+    './src/**/*.js',
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
