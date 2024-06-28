889726a5a530eec0d67349c20c89024918e3da5f
developer2031997 <168425489+developer2031997@users.noreply.github.com>
2024-06-28 11:54:33 +0530

    new

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  css: [
+    // '~~/layout/main.css',
+    '@/assets/css/tailwind.css',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  build: {
+    transpile: [],
+
+  },
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/tailwindcss'
+  ],
+  googleFonts: {
+    families: {
+      Poppins: [300, 400, 500, 600, 700],
+    },
+    // display: 'swap', // Optional: ensures text remains visible during webfont load
+  },
+  content: {
+  },
+  plugins: [
+  ],
+})
